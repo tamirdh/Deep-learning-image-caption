a7eea4c7d1a5537c5bb50277538ddaab111d8537
Tamir David Hay <>
2021-09-05 00:21:09 +0300

    Hidden change

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ if __name__ == '__main__':
     data_loader = get_dataloader(dataset, args.batch, shuffle=shuffle)
     vocab_size = len(dataset.vocab)
     embed_size = vocab_size//2
-    hidden_size = 100
+    hidden_size = vocab_size//5
     print(f"Embed size:{embed_size}\nHidden size:{hidden_size}")
     model = CNNtoRNN(embed_size, hidden_size, vocab_size)
-    
+    print(model)
     if args.overfit:
         overfit(model, device, data_loader, args.T)
         #model.load_state_dict(torch.load("checkpoint.torch")["model_state_dict"])
